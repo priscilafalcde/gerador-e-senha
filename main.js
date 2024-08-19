807d5d46d7c980e284d8a39e17cb27a352d99bce
priscilafalcde <116758378+priscilafalcde@users.noreply.github.com>
2024-08-19 15:31:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const numeroSenha = document.querySelector('.parametro-senha_texto');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
-const letraMinusculas = 'abcefghijklmnopqrstuvxywz';
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
 const numeros = '0123456789';
 const simbolos = '!@%*?';
-const botoes = document.querySelectorAll('parametro-senha_botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 const forcaSenha = document.querySelector('.forca');
@@ -14,7 +14,7 @@ botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho() {
-    if (tamanhoSenha > 1) {
+    if (tamanhoSenha>1) {
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
@@ -33,9 +33,10 @@ function aumentaTamanho() {
 for (i = 0; i < checkbox.length; i++) {
     checkbox[i].onclick = geraSenha;
 }
+
 geraSenha();
 function geraSenha() {
-    let alfabeto = "";
+    let alfabeto ='';
     if (checkbox[0].checked) {
         alfabeto = alfabeto + letrasMaiusculas;
     }
@@ -49,7 +50,7 @@ function geraSenha() {
     if (checkbox[3].checked) {
         alfabeto = alfabeto + simbolos;
     }
-    let senha = "";
+    let senha ='';
     for (let i = 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random() * alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
@@ -72,6 +73,8 @@ function classificaSenha(tamanhoAlfabeto) {
     const valorEntropia = document.querySelector('.entropia');
     valorEntropia.textContent = 2 ** Math.floor(entropia) / (100e6 * 60 * 60 * 24)
 }
+
+
 
 
 
